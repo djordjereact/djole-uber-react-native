6a1146e7df6328ab8757681942705849ed8ba1ef
dusan <duks05@yahoo.com>
2023-06-17 16:43:11 +0200

    Implemented redux

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets build UBER!</Text>
-    </View>
+      <Provider store={store}>
+          <View style={styles.container}>
+            <Text>Lets build UBER!</Text>
+          </View>
+      </Provider>
+
   );
 }
 
