052d4c90d5b9796891a7f462733e2670ccd31b67
dusan <duks05@yahoo.com>
2023-06-18 22:56:46 +0200

    Added NavOptions file

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import { Provider } from "react-redux";
 import { store } from "./store";
 import HomeScreen from "./screens/HomeScreen";
+import NavOptions from "./components/NavOptions";
 
 export default function App() {
   return (
       <Provider store={store}>
-          <HomeScreen />
+          <View>
+              <HomeScreen />
+              <NavOptions />
+          </View>
       </Provider>
 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
