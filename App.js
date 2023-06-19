1312dcc81ce6a3cc1474ebce2674d22c39f81682
dusan <duks05@yahoo.com>
2023-06-19 11:00:47 +0200

    Added navigation

--- App.js
+++ App.js
@@ -6,13 +6,26 @@ import HomeScreen from "./screens/HomeScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from "./screens/MapScreen";
 
 export default function App() {
+    const Stack = createNativeStackNavigator();
+
   return (
       <Provider store={store}>
           <NavigationContainer>
               <SafeAreaProvider>
-                  <HomeScreen />
+                  <Stack.Navigator>
+                      <Stack.Screen
+                          name="HomeScreen"
+                          component={HomeScreen}
+                          options={{headerShown: false}}/>
+                      <Stack.Screen
+                          name="MapScreen"
+                          component={MapScreen}
+                          options={{headerShown: false}}/>
+                  </Stack.Navigator>
               </SafeAreaProvider>
           </NavigationContainer>
       </Provider>
